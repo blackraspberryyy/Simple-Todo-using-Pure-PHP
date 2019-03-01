05fa5a771f776858339532ac35e649bc2629dbda
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2019-03-01 23:05:32 +0800

    Remove unnecessary thingys

--- index.php
+++ index.php
@@ -18,54 +18,7 @@
     </style> -->
   </head>
   <body>
-    <!-- Nav Bar -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-dark">
-        <a class="navbar-brand" href="#">Jessu's Cuisine</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">SIGN IN</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">CREATE ACCOUNT</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
-    <div class="container">
-      <div class="row mt-3">
-        <div class="col-sm-8 offset-sm-2">
-          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="img1.jpg" class="d-block w-100" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="img2.jpg" class="d-block w-100" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="img3.jpg" class="d-block w-100" alt="...">
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-        </div>
-      </div>
-
+    <div class="container" style="margin-top:32px;">
       <h1>Todo list!</h1>
 
       <button type="button" class="btn btn-success" style="margin-bottom:16px;" data-toggle="modal" data-target="#exampleModal">+ Add Todo</button>
@@ -136,7 +89,6 @@
           <?php endwhile;?>
         </tbody>
       </table>
-
     </div>
 
     <!-- Create Modal -->
