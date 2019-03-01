9f4e81d02dec7fb8107c702de83b870623241f2a
Juan Carlo Valencia <carlo.valencia066@gmail.com>
2019-03-02 01:15:37 +0800

    Removed comments

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!DOCTYPE>
 <!DOCTYPE html>
 <html>
   <head>
@@ -9,15 +8,9 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <!-- <style>
-      .col-sm, .col-sm-6, .col-sm-1, .col-sm-4, .col-sm-3{
-        background: #ccc;
-        padding:20px;
-        border:1px solid red;
-      }
-    </style> -->
   </head>
   <body>
+    <?php include('nav.php');?>
     <div class="container" style="margin-top:32px;">
       <h1>Todo list!</h1>
 
